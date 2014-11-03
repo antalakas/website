66322921b993f3d7d489a0b96e1816fe9edfa4ed
Andreas Ntalakas <antalakas@gmail.com>
2014-11-03 14:29:37 +0000

    customizations for Brussels

--- seo.js
+++ seo.js
@@ -1,9 +1,9 @@
 Meteor.startup(function() {
   if(Meteor.isClient) {
     SEO.config({
-      title: 'Meteor Vegas',
+      title: 'Meteor Brussels',
       meta: {
-        'description': 'A Bi-Monthly Meteor.js Meetup in Las Vegas, NV'
+        'description': 'A Bi-Monthly Meteor.js Meetup in Brussels, Belgium'
       },
       og: {
         'image': Meteor.absoluteUrl('share-image.png')
